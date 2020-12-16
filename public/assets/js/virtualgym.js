f7c6660c51af3851b7fb2a645bf68ad5f99677a1
TChristensenDrumz <tchristensendrumz@gmail.com>
2020-12-16 10:56:32 -0800

    async functionality started

--- public/assets/js/virtualgym.js
+++ public/assets/js/virtualgym.js
@@ -1,64 +1,82 @@
 $(document).ready(function() {
     const routine = $("#virtualgym");
+    const timerSent = $("#timerSent");
 
     $.get("/api/user_data").then(function(data) {
-        getRoutine(data);
+        virtualGym(data);
     });
 
-    function getRoutine(data) {
+    async function virtualGym(data) {
         const serial = data.equipment;
         const equipment = serial.split("-").filter(element => element.length > 0);
         let urlParams = "";
-
+    
         for (let i = 0; i < equipment.length; i++) {
             urlParams += "&equipment=" + equipment[i];
         }
-
+    
         var queryURL = "https://wger.de/api/v2/exercise/?language=2&limit=50" + urlParams;
-
+    
         $.ajax({
             url: queryURL,
             method: "GET",
             Authorization: "b848c6ad024b1c40f59e4da17743e3a1c17d613e"
-        }).then(function(res) {
+        }).then(async function(res) {
             const results = res.results;
-
-            for (let i = 0; i < results.length; i++) {
-                let name = results[i].name;
-                let description = results[i].description;
-                let category = results[i].category;
-
-                let div = $("<div>")
-
-                let divName = $("<div>").text(name)
-                let divDescription = $("<p>").html(description)
-                let divCategory = $("<div>").text(category)
-                startExercise(div, divName, divDescription, divCategory);
-            }
-
-            //Add Timer function
-            async function startExercise(div, divName, divDescription, divCategory) {
-                const timerSent = $("#timerSent")
-                displayExercise(div, divName, divDescription, divCategory);
-                let timer = 20;
-                var timerInterval = setInterval(function() {
-                    timer--;
-                    timerSent.text(timer + " seconds left");
-                    if (timer <= 0) {
-                        clearInterval(timerInterval);
-                        //Clear current exercise
-                        routine.text("");
+    
+            for(let i = 0; i < results.length; i++) {
+                const workout = await generateHTML(results, i);
+                routine.append(workout);
+                for(let j = 0; j < 3; j++) {
+                    const set = await workoutSet();
+                    if(j < 2) {
+                        const rest = await workoutRest();
                     }
-                }, 1000);
-            }
-
-            //Append
-            function displayExercise(div, divName, divDescription, divCategory) {
-                div.append(divName)
-                div.append(divDescription)
-                div.append(divCategory)
-                routine.append(div);
+                }
+                routine.text("");
             }
         })
     }
+    
+    function generateHTML(data, i) {
+        const name = data[i].name;
+        const description = data[i].description;
+        const category = data[i].category;
+    
+        const div = $("<div>")
+    
+        const divName = $("<div>").text(name)
+        const divDescription = $("<p>").html(description)
+        const divCategory = $("<div>").text(category)
+    
+        div.append(divName);
+        div.append(divDescription);
+        div.append(divCategory);
+    
+        return div;
+    }
+    
+    function workoutSet() {
+        let timer = 20;
+        const timerInterval = setInterval(function() {
+            timer--;
+            timerSent.text("Workout " + timer + " seconds left");
+            if (timer <= 0) {
+                clearInterval(timerInterval);
+                return;
+            }
+        }, 1000);
+    }
+    
+    function workoutRest() {
+        let timer = 10;
+        const timerInterval = setInterval(function() {
+            timer--;
+            timerSent.text("Rest " + timer + " seconds left");
+            if (timer <= 0) {
+                clearInterval(timerInterval);
+                return;
+            }
+        }, 1000);
+    }   
 });
